014f758ca8aa54167dc50015d577fedbed011dc9
Levi-1103 <58707841+Levi-1103@users.noreply.github.com>
2024-10-19 03:14:13 +0100

    updated agg command to fetch feeds automatically during an interval

--- handler_agg.go
+++ handler_agg.go
@@ -2,20 +2,65 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"time"
+
+	"github.com/Levi-1103/gator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	if len(cmd.args) != 0 {
-		return fmt.Errorf("command has no arguments")
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("command requires argument: agg <time_between_reqs> example: 1h")
 	}
 
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	timeBetweenRequests, err := time.ParseDuration(cmd.args[0])
 	if err != nil {
-		return fmt.Errorf("couldn't fetch feed request: %w", err)
+		return fmt.Errorf("not a valid time string")
 	}
 
-	fmt.Println(feed)
+	fmt.Println("Collecting feeds every: ", cmd.args[0])
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		go scrapeFeeds(s)
+	}
+
+}
+
+func scrapeFeeds(s *state) error {
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return err
+	}
+
+	now := time.Now().UTC()
+	nullTime := sql.NullTime{
+		Time:  now,
+		Valid: true,
+	}
+
+	err = s.db.MarkFeedFetched(context.Background(), database.MarkFeedFetchedParams{
+		ID:            nextFeed.ID,
+		LastFetchedAt: nullTime,
+		UpdatedAt:     now,
+	})
+	if err != nil {
+		return err
+	}
+
+	feedData, err := fetchFeed(context.Background(), nextFeed.Url)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println()
+
+	for _, item := range feedData.Channel.Item {
+		fmt.Println(item.Title)
+	}
+
+	fmt.Println()
 
 	return nil
 }
